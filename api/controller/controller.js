9bcd4b783f1a8dbc5f1d3341dabe46b0a7129381
stan-2021131 <sergiocoromac@gmail.com>
2026-03-19 20:45:58 -0600

    Se agregó la ruta put del api

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -53,3 +53,24 @@ export const nuevoVideojuego = async (req, res) => {
 
   res.status(201).json(result.rows[0]);
 };
+
+export const actualizarVideojuego = async (req, res) => {
+  const error = validacion(req.body);
+  if (error) return res.status(400).json({ error });
+  const id = req.params.id;
+
+  const { campo1, campo2, campo3, campo4, campo5, campo6 } = req.body;
+
+  const result = await pool.query(
+    `UPDATE videojuegos
+     SET campo1=$1, campo2=$2, campo3=$3, campo4=$4, campo5=$5, campo6=$6
+     WHERE id=$7 RETURNING *`,
+    [campo1, campo2, campo3, campo4, campo5, campo6, id],
+  );
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: "Videojuego no encontrado" });
+  }
+
+  res.json(result.rows[0]);
+};
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { obtenerVideojuegos, obtenerXId, nuevoVideojuego } from '../controller/controller.js';
+import { obtenerVideojuegos, obtenerXId, nuevoVideojuego, actualizarVideojuego } from '../controller/controller.js';
 const router = Router();
 
 router.get('/', obtenerVideojuegos);
 router.get("/:id", obtenerXId);
 router.post("/", nuevoVideojuego);
+router.put("/:id", actualizarVideojuego);
 
 export default router;
