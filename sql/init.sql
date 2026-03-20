55420f7dffd009c9c9f2ab14a6c31378ec6b5fae
stan-2021131 <sergiocoromac@gmail.com>
2026-03-19 20:13:20 -0600

    Se realizaron cambios en el docker-compose para levantar los tres contenedores necesarios y se agregó el sql con los datos necesarios para la bd

---
+++ sql/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE videojuegos (
+    id SERIAL PRIMARY KEY,
+    campo1 TEXT NOT NULL,
+    campo2 TEXT NOT NULL,
+    campo3 TEXT NOT NULL,
+    campo4 INTEGER NOT NULL,
+    campo5 REAL NOT NULL,
+    campo6 BOOLEAN NOT NULL
+);
