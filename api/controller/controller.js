8c821399a84e32d4c5c194bd630a0a25a97902b0
stan-2021131 <sergiocoromac@gmail.com>
2026-03-19 20:50:01 -0600

    Ruta delete agregada

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -74,3 +74,17 @@ export const actualizarVideojuego = async (req, res) => {
 
   res.json(result.rows[0]);
 };
+
+export const eliminarVideojuego = async (req, res) => {
+  const id = req.params.id;
+  const result = await pool.query(
+    "DELETE FROM videojuegos WHERE id=$1 RETURNING *",
+    [id],
+  );
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: "Videojuego no encontrado" });
+  }
+
+  res.status(204).send();
+};
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { obtenerVideojuegos, obtenerXId, nuevoVideojuego, actualizarVideojuego } from '../controller/controller.js';
+import { obtenerVideojuegos, obtenerXId, nuevoVideojuego, actualizarVideojuego, eliminarVideojuego } from '../controller/controller.js';
 const router = Router();
 
 router.get('/', obtenerVideojuegos);
 router.get("/:id", obtenerXId);
 router.post("/", nuevoVideojuego);
 router.put("/:id", actualizarVideojuego);
+router.delete("/:id", eliminarVideojuego);
 
 export default router;
